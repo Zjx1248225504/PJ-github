4c96a2a21f307d1e30daf1ce7dc11f08727e1f9e
张小萌 <1248225504@qq.com>
2017-08-11 19:14:06 +0800

    2017/8/11 张小萌

--- TP-composer/tp5/application/index/controller/Index.php
+++ TP-composer/tp5/application/index/controller/Index.php
@@ -1,10 +1,11 @@
 <?php
 	//创建命名空间
+	//命名空间将代码划分出不同的空间（区域），每个空间的常量、函数、类（为了偷懒，我下边都将它们称为元素）的名字互不影响
 	namespace app\index\controller;
-	//定义Index类
+	//定义Index类,类名需要和文件名保持一致
 	class Index {
 		public function index() {
-			
+			return "this index Index index";
 		} 
 	}
 ?>
