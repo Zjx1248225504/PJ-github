b09a32e8b61e95321a3a3e8d44f6330abfff216d
张小萌 <1248225504@qq.com>
2017-08-16 09:25:10 +0800

    cad 2017/8/16

--- TP-composer/tp5/application/admin/controller/Index.php
+++ TP-composer/tp5/application/admin/controller/Index.php
@@ -1,9 +1,13 @@
 <?php
-	namespace app\admin\controller
-	
+	namespace app\admin\controller;
+	use app\common\controller\Index as commonIndex;
 	class Index {
 		public function index() {
 			return "this is admin index Index";
+		}
+		public function common() {
+			$common = new commonIndex();
+			return $common->index();
 		}
 	}
 	
--- TP-composer/tp5/application/index/controller/Index.php
+++ TP-composer/tp5/application/index/controller/Index.php
@@ -2,10 +2,15 @@
 	//创建命名空间
 	//命名空间将代码划分出不同的空间（区域），每个空间的常量、函数、类（为了偷懒，我下边都将它们称为元素）的名字互不影响
 	namespace app\index\controller;
+	use app\common\controller\Index as commonIndex;
 	//定义Index类,类名需要和文件名保持一致
 	class Index {
 		public function index() {
 			return "this index Index index";
 		} 
+		public function common() {
+			$common = new commonIndex();
+			return $common->index();
+		}
 	}
 ?>
