b58050426b7cfaf2e821d3dfc8379590a9e5082f
张小萌 <1248225504@qq.com>
2017-08-30 17:58:11 +0800

    FM 张小萌

--- FM/dir.func.php
+++ FM/dir.func.php
@@ -1,22 +1,32 @@
 <?php
-//存放目录相关操作函数
+	$path = '/file/';
+//	if ($dh = opendir($path)){
+//	    while (($file = readdir($dh)) !== false){
+//	    	if($file != "." && $file != ".."){
+//	    		echo "filename:" . $file . "<br>";
+//	    	}
+//	    }
+//	    closedir($dh);
+//	}
+	
 	function readDirectory(){
 		//打开指定目录,遍历目录函数
 		$handle = opendir($path);
-		while (($item = readdir($handle)) !== file){
-			//屏蔽.与..这两个特殊字符
-			if($item != "." && $item != ".."){
-				if(is_file($path."/".$item)){
-					$arr['file'][] = $item; 
-				}	
-				if(is_dir($path."/".$item)){
-					$arr['dir'][] = $item; 
-				}
-			}
-		}
-		closedir($handel);
-		return $arr;
+//		while (($item = readdir($handle)) !== false){
+//			//屏蔽.与..这两个特殊字符
+//			if($item != "." && $item != ".."){
+//				if(is_file($path."/".$item)){
+//					$arr['file'][] = $item; 
+//				}	
+//				if(is_dir($path."/".$item)){
+//					$arr['dir'][] = $item; 
+//				}
+//			}
+//		}
+//		closedir($handel);
+//		return $arr;
 	}
-	$path = __FILE__;
-	print_r(readDirectory($path));
+	
+	readDirectory();
+	//print_r(readDirectory($path));
 ?>
