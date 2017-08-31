253a768736f218a0bc5aac3219ed18943bc5a0da
张小萌 <1248225504@qq.com>
2017-08-31 11:57:08 +0800

    FM 张小萌

--- FM/dir.func.php
+++ FM/dir.func.php
@@ -1,32 +1,21 @@
 <?php
-	$path = '/file/';
-//	if ($dh = opendir($path)){
-//	    while (($file = readdir($dh)) !== false){
-//	    	if($file != "." && $file != ".."){
-//	    		echo "filename:" . $file . "<br>";
-//	    	}
-//	    }
-//	    closedir($dh);
-//	}
-	
-	function readDirectory(){
+	function readDirectory($path){
 		//打开指定目录,遍历目录函数
 		$handle = opendir($path);
-//		while (($item = readdir($handle)) !== false){
-//			//屏蔽.与..这两个特殊字符
-//			if($item != "." && $item != ".."){
-//				if(is_file($path."/".$item)){
-//					$arr['file'][] = $item; 
-//				}	
-//				if(is_dir($path."/".$item)){
-//					$arr['dir'][] = $item; 
-//				}
-//			}
-//		}
-//		closedir($handel);
-//		return $arr;
+		while (($item = readdir($handle)) !== false){
+			//屏蔽.与..这两个特殊字符
+			if ($item != "." && $item != ".."){
+				if (is_file ($path . "/" . $item)){
+					$arr ['file'] [] = $item; 
+				}	
+				if (is_dir ($path . "/" . $item)){
+					$arr ['dir'] [] = $item; 
+				}
+			}
+		}
+		closedir ($handle);
+		return $arr;
 	}
-	
-	readDirectory();
-	//print_r(readDirectory($path));
+//	$path = 'file';
+//	print_r(readDirectory($path));
 ?>
--- FM/index.php
+++ FM/index.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
+<?php
+	require_once 'dir.func.php';
+	$path="file";
+	$info=readDirectory($path);
+	//print_r($info);
+?>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<title>文件管理器</title>
+		<link rel="stylesheet" href="css/CssReset.css">
+			<link rel="stylesheet" href="css/index.css">
 		<!--<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"> -->
 	</head>
 	<body>
-		
+		<h1>在线文件管理</h1>
+		<table width="100%" border="1" cellpadding="5" cellspacing="0" bgcolor="#FFFFFF" align="center">
+			<tr>
+				<td>编号</td>
+				<td>名称</td>
+				<td>类型</td>
+				<td>大小</td>
+				<td>可读</td>
+				<td>可写</td>
+				<td>可执行</td>
+				<td>创建时间</td>
+				<td>修改时间</td>
+				<td>访问时间</td>
+				<td>操作</td>
+			</tr>
+			<?php
+				if($arr['file']){
+					foreach()
+				}
+			?>
+		</table>
 		
 	</body>
 </html>
