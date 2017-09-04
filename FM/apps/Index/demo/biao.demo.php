5ed697cdf0748311444c52dbf063d40bd7e86715
张小萌 <1248225504@qq.com>
2017-09-04 15:00:33 +0800

    FM 张小萌

--- FM/apps/Index/demo/biao.demo.php
+++ FM/apps/Index/demo/biao.demo.php
@@ -10,8 +10,17 @@
 	<!--大小-->
 	<td><?php echo $Func -> Dw(filesize($src));?></td>
 	<!--文件可读-->
-	<td><?php if(is_readable($src)){echo "可读";} else {echo "不可读";} ?></td>
+	<td><?php if(is_readable($src)){echo '<img width="20%" src="images/correct.ico.png"/>';} else {echo '<img width="20%" src="images/error.ico.png"/>';}?></td>
 	<!--文件可写-->
-	
+	<td><?php if(is_writable($src)){echo '<img width="20%" src="images/correct.ico.png"/>';} else {echo '<img width="20%" src="images/error.ico.png"/>';}?></td>
 	<!--文件可执行-->
+	<td><?php if(is_executable($src)){echo '<img width="20%" src="images/correct.ico.png"/>';} else {echo '<img width="20%" src="images/error.ico.png"/>';}?></td>
+	<!--文件创建时间-->
+	<td><?php echo date("Y-m-d H:i:s",filectime($src));?></td>
+	<!--文件修改时间-->
+	<td><?php echo date("Y-m-d H:i:s",filemtime($src));?></td>
+	<!--文件访问时间-->
+	<td><?php echo date("Y-m-d H:i:s",fileatime($src));?></td>
+	<!--操作-->
+	<td><?php echo 1;?></td>
 </tr>
